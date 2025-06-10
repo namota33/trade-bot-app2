0310d7f02d98156944caa8727810ee942a018453
namota33 <nataccb2@gmail.com>
2025-06-10 11:37:34 +0000

    fix: adiciona @vitejs/plugin-react

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './', // ou remova essa linha se não estiver usando subpasta como root
-  build: {
-    outDir: 'dist',
-  },
-})
+  })
