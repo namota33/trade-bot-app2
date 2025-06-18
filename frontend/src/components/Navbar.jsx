a1e61d6a1239d9f3a10309797df87c069038cb10
namota33 <nataccb2@gmail.com>
2025-06-18 19:26:21 +0000

    alterando rota

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav style={{ padding: '10px', background: '#ddd' }}>
       <Link to="/" style={{ marginRight: '15px' }}>Home</Link>
-      <Link to="/dashboard" style={{ marginRight: '15px' }}>Dashboard</Link>
+      <Link to="/Dashboard" style={{ marginRight: '15px' }}>Dashboard</Link>
       <Link to="/config">Configurações</Link>
     </nav>
   );
